aadc921e12d94f49fe7a12b8c7622e21e34fdbde
Coleman Imhoff <colemanimhoff@gmail.com>
2018-03-18 16:59:41 -0600

    first commit

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 Starting code is in the 'master' branch
 
 Finished code is in the 'breakout-finished' branch
+
+##Objectives
+
+* Use fetch to GET data from an API
+* Use fetch to POST data to an API
+* Explain the use case for Fetch
+* Look up some public APIs we can use
+
+##Notes
+
+* What is a fetch?
+- GET Request (I want some data!)
+  - Request for information from an API (server & database)
+  - Server: querying a database for information
+- POST Request
--- app.js
+++ app.js
@@ -1,6 +1,49 @@
-// GET request
+// GET request (Give me data!)
   // Request to get information from an API
   // Different API endpoints => different data returned
+  // One argument => the url you are requesting data from
 
-var baseUrl = 'https://swapi.co/api/'
+const baseUrl = 'https://swapi.co/api/'
 
+fetch(`${baseUrl}people/2`)  //Asynchronous request
+.then(function (response) {   //Wait for respone to continue
+  return response.json()   //Turn response into json and return it to next .then
+})
+.then(function (data) {
+  console.log(data)
+  let name = data.name   //Do something with the data; often DOM manipulation
+  let cardTitle = document.querySelector('.card-title')
+  cardTitle.textContent = name
+})
+.catch(function(error) {
+  console.log(error)
+})
+
+//POST request
+  //We HAVE some information and we send it to the API
+  //Typically user interaction through form
+  //This code won't work... it's just an example
+
+// let userName = document.querySelector('#user-name').value
+// let address = document.querySelector('#address').value
+// let postObject = {
+//   name: userName,
+//   address: address
+// }
+//
+// let postOptions = {
+//   method: 'POST',
+//   headers: new Headers({'Content-Type': 'application/json'})
+//   body: JSON.stringify(postObject)
+// }
+//
+// fetch(`${baseUrl}submit`)
+// .then(function(response){
+//   return response.json()
+// })
+// .then(function(data){
+//   console.log(data);
+// })
+// .catch(function(error){
+//   console.log(error);
+// })
